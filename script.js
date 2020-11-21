09c5283c428f5c0eb6c22e49fb7f36f547a039ba
Antonne Turner <antonneturner@gmail.com>
2020-11-21 13:49:35 -0500

    add time clock in jquery

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ $('.form-control').each(function () {
     console.log($(this))
 
 })
+
+// time 
+
+$(document).ready(function () {
+    var today = moment();
+    $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+})
