c7561b49f880f5b647e5467393ae1920ba9d0ac2
Antonne Turner <antonneturner@gmail.com>
2020-11-22 22:24:26 -0500

    add var index array, add for var index, if military current hour, if military future and past hour

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ $('.form-control').each(function () {
 
 // time 
 
+
+
+var militaryHour = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
 $(document).ready(function () {
     var today = moment();
+    var currentHour = moment().hour()
     $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+
+    for (var i = 0; i < militaryHour.length; i++) {
+        if (militaryHour[i] === currentHour) {
+            $("#" + i).addClass("present")
+        }
+        if (militaryHour[i] > currentHour) {
+            $("#" + i).addClass("future")
+
+        }
+        if (militaryHour[i] < currentHour) {
+            $("#" + i).addClass("past")
+
+        }
+    }
 })
+
+
+
+
